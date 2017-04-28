91c9db43cb426714fc192a4e4b6a6935091236ca
Tao He <longlinht@gmail.com>
2017-04-28 14:40:12 +0800

    init project

---
+++ app/src/main/java/io/github/longlinht/taobrowser/TaoBrowserActivity.java
@@ -0,0 +1,13 @@
+package io.github.longlinht.taobrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaoBrowserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tao_browser);
+    }
+}
